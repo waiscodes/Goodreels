686ab3ce73fad4cb8efd60fc4db27c71f4cd14b6
waiscodes <b.wais@outlook.com>
2020-09-21 13:27:43 -0600

    ADDED form inputs and an event listener

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import '../css/SignIn.css';
-
+import React from "react";
+import "../css/SignIn.css";
 
 const SignIn = () => {
+  const SumbitEvent = (e) => {
+    // e.preventDefault();
+  };
 
+  return (
+    <>
+      <h2>Signup</h2>
+      <form onSubmit={SumbitEvent}>
+        <label htmlFor="email">Email: </label>
+        <input type="email" name="email" id="email" />
 
-    return (
+        <label htmlFor="username">Username: </label>
+        <input type="text" name="username" id="username" />
 
-        <>
-        </>
-
-    );
-    
-}
+        <input type="submit" value="Sumbit" />
+      </form>
+    </>
+  );
+};
 export default SignIn;
