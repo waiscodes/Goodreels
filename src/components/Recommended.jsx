23b3f96c07899a45fd35d894cffe7e0594c16096
AdolfStary <adolf.stary@hotmail.com>
2020-09-24 16:21:02 -0600

    ADDED redirect to Recommended page if not logged in;

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -2,27 +2,35 @@ import React from "react";
 import MovieCard from "./MovieCard";
 import "../css/Recommended.css";
 import { connect } from "react-redux";
+import { Redirect } from 'react-router-dom';
 
 const Recommended = (props) => {
 
-  return (
-    <>
-      <ul>
-        {props.movies.map((movie) => {
-          if (props.movies.indexOf(movie) < 5){
-            return (
-              <li key={movie.id}>
-                <MovieCard movieId={movie.id} />
-              </li>
-            );
-          }
-          else return "";
-          
-        })}
-      </ul>
-    </>
+  if (props.activeUser.username !== undefined){
+    return (
+      <>
+        <ul>
+          {props.movies.map((movie) => {
+            if (props.movies.indexOf(movie) < 5){
+              return (
+                <li key={movie.id}>
+                  <MovieCard movieId={movie.id} />
+                </li>
+              );
+            }
+            else return "";
+            
+          })}
+        </ul>
+      </>
+    );
+  }
+  else return (
+    <Redirect to="/Browse" />
   );
+  
 };
 export default connect((state) => ({
   movies: state.movies,
+  activeUser: state.activeUser
 }))(Recommended);
