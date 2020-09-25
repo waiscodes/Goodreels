07706224ed19d8f51aa878cf88ee5736d77a4f1e
waiscodes <b.wais@outlook.com>
2020-09-25 14:04:37 -0600

    added flex to browse

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,43 +1,41 @@
-
-import React from 'react';
-import ListReviews from './ListReviews';
-import ListMovies from './ListMovies';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import '../css/Browse.css';
-import { useState } from 'react';
-
+import React from "react";
+import ListReviews from "./ListReviews";
+import ListMovies from "./ListMovies";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import "../css/Browse.css";
+import { useState } from "react";
 
 const Browse = (props) => {
+  let [searchTerm, updateSearch] = useState("");
 
-    let [searchTerm, updateSearch] = useState('');
+  if (props.activeUser.username !== undefined) {
+    return (
+      // Search, filter
+      <>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <label htmlFor="movie-search" className="screen-reader-text">
+            Enter Movie Name
+          </label>
+          <input
+            id="movie-search"
+            type="search"
+            onChange={(e) => updateSearch(e.target.value)}
+            value={searchTerm}
+            placeholder="Search Movie"
+          />
+        </form>
 
-    if (props.activeUser.username !== undefined){
-
-        
-
-        return (
-            // Search, filter
-            <>
-                <form onSubmit={event => { event.preventDefault(); }}>
-                    <label htmlFor="movie-search" className="screen-reader-text">Enter Movie Name</label>
-                    <input
-                        id="movie-search"
-                        type="search"
-                        onChange={e => updateSearch(e.target.value)}
-                        value={searchTerm}
-                        placeholder="Search Movie"
-                    />
-                </form>
-                
-                <ListMovies searchTerm={searchTerm}/>
-                
-
-
-            </>
-        );
-    }
-    else return <Redirect to="/" />
+        <section className="main-section">
+          <ListMovies searchTerm={searchTerm} />
+        </section>
+      </>
+    );
+  } else return <Redirect to="/" />;
 };
 
 export default connect((state) => ({ activeUser: state.activeUser }))(Browse);
