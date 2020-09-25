35b2a29e9d19df99655b36d8f840b68a746265b1
waiscodes <b.wais@outlook.com>
2020-09-25 09:11:18 -0600

    added screen reader class, and cleaned up

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,39 +1,30 @@
-import React from 'react';
-import ListReviews from './ListReviews';
-import ListMovies from './ListMovies';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import '../css/Browse.css';
-import { setState } from 'react';
-
+import React from "react";
+import ListReviews from "./ListReviews";
+import ListMovies from "./ListMovies";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import "../css/Browse.css";
+import { setState } from "react";
 
 const Browse = (props) => {
+  if (props.activeUser.username !== undefined) {
+    return (
+      // Search, filter
+      <>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <label htmlFor="movie-search" className="screen-reader-text">
+            Enter Movie name
+          </label>
+          <input id="movie-search" type="search" placeholder="Search Movie" />
+        </form>
 
-
-
-    if (props.activeUser.username !== undefined){
-
-        return (
-            // Search, filter
-            <>
-                <form onSubmit={event => { event.preventDefault(); }}>
-                    <label htmlFor="movie">Enter Movie name</label>
-                    <input
-                        id="movie"
-                        type="search"
-                    />
-                </form>
-                
-                <ListMovies />
-                
-
-
-            </>
-        );
-    }
-    else return (
-            <Redirect to="/" />
+        <ListMovies />
+      </>
     );
-
-}
-export default connect(state =>({activeUser: state.activeUser}))(Browse);
+  } else return <Redirect to="/" />;
+};
+export default connect((state) => ({ activeUser: state.activeUser }))(Browse);
