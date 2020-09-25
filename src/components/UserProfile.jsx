53b0a53cc4c7f1d9a66265f1fd9a41d2b614682c
AdolfStary <adolf.stary@hotmail.com>
2020-09-24 20:32:33 -0600

    FIXED errors, cleaned up commented out code, organized code a little, ADDED few safety features for possible future errors;

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,68 +1,80 @@
-import { render } from '@testing-library/react';
-import React,{useState} from 'react';
 
-import '../css/UserProfile.css';
+import React,{useState} from 'react';
 import Logo from '../images/avatar2.png';
 import { updateProfile } from '../actions/aProfile';
 import ListReviews  from './ListReviews';
 import { connect } from 'react-redux';
 import { logInUser } from '../actions/aActiveUser';
+import { Redirect } from 'react-router-dom';
+
+import '../css/UserProfile.css';
 
 
 
 
 
 
-function UserProfile(props) {
-    let thisUser;
+const UserProfile = (props) => {
+
+    let thisUser = {};
+
     for (const element of props.users) {
         if (element.username === props.activeUser.username)  
               thisUser = { ...element }; 
-            }  
+    }  
         
-    const [username, setUserName] = useState(thisUser.username);
-    const [email, setEmail] = useState(thisUser.email);
-    const [password, setPassword] = useState(thisUser.password);
-    //const [bio, setBio] = useState('');
-        const activeUser = props.activeUser.username;
-    //this below state needs to be fixed
+    let [username, setUserName] = useState(thisUser.username);
+    let [email, setEmail] = useState(thisUser.email);
+    let [password, setPassword] = useState(thisUser.password);
+    let [bio, setBio] = useState(thisUser.bio);
+    let fileUrl = "";
+    
     
 
     const handleSubmit = event => {
         event.preventDefault();
+
+        let isValidUsername = true;
         
         const usrPEmail = document.querySelector("#user-email").value;
         const usrPUsername = document.querySelector("#username-input").value;
         const usrPPassword = document.querySelector("#user-Password").value;
-        const usrPBio = document.querySelector("#userBio").value;
-        //alert(`${ usrPBio }  ${ usrPUsername }  ${ usrPEmail }`);
+        const usrPBio = document.querySelector("#userBio").value;  
 
-        
-        // type: "UPDATE_PROFILE",
-       // oldUsername,
-       // username,
-       // email,
-       // password,
-       // bio,
-        //image
-        
+        for (const user of props.users){
+            if (user.username === usrPUsername) isValidUsername = false;
+        }
 
-        props.dispatch(updateProfile(activeUser, usrPUsername, usrPEmail, usrPPassword, usrPBio));
-       // props.dispatch(logInUser(usrPUsername, usrPUsername, usrPEmail, usrPPassword, usrPBio));
+        if (isValidUsername){
+
+            props.dispatch(updateProfile(props.activeUser.username, usrPUsername, usrPEmail, usrPPassword, usrPBio, fileUrl));
+            props.dispatch(logInUser(usrPUsername));
+
+        }
+        else alert("This username is already taken. Please use different username.");
+        
         
 
     }
         
     
-        const loadFile = function (event) {
-            const image = document.getElementById('output');
-            image.src = URL.createObjectURL(event.target.files[0]);
-    
+    const loadFile = function (event) {
+        const image = document.getElementById('output');
+        if (event.target.files[0] !== undefined)
+        {
+            fileUrl = URL.createObjectURL(event.target.files[0]);
+            image.src = fileUrl;
         }
-     
+        
+
+    }
+    
+    if (props.activeUser.username !== undefined){
+
+  
         return (
 
-            <div>
+            <>
                 <h1 className="profile-head">User Profile</h1>
          
                 <form onSubmit={handleSubmit} >
@@ -75,55 +87,63 @@ function UserProfile(props) {
                     </div>
                     <div className="container">
                         <div className="">
-                            <label htmlFor="username-input"    
-                                className="labels"
-                                type="text">Username</label> <br />
-                            <input id="username-input"
+                            <label htmlFor="username-input" className="labels" type="text">Username</label> <br />
+                            <input 
+                                id="username-input"
                                 htmlFor="username-input"
                                 className="inputs"
                                 value={username}
-                             placeholder="Your Username"
-                                type="text"
-                                
-                            onChange={event => { setUserName(event.target.value); }}
+                                placeholder="Your Username"
+                                type="text" 
+                                onChange={event => { setUserName(event.target.value); }}
                             />
                         </div>
                         <div >
                             <label htmlFor="user-email" className="labels" type="text">Email</label><br />
-                            <input id="user-email" className="inputs"
+                            <input 
+                                id="user-email" 
+                                className="inputs"
                                 placeholder="Your email...." type="text"
                                 value={email}
-                             onChange={event => { setEmail(event.target.value); }}
+                                onChange={event => { setEmail(event.target.value); }}
                             />
                         </div>
                         <div >
                             <label htmlFor="user-password" className="labels" type="text">Change Password</label><br />
-                            <input id="user-Password" className="inputs"
+                            <input 
+                                id="user-Password" 
+                                className="inputs"
                                 placeholder="" type="password"
                                 value={password}
-                             onChange={event => { setPassword(event.target.value); }}
+                                onChange={event => { setPassword(event.target.value); }}
                             />
-                        </div>
-                        
+                        </div> 
                         <div className="">
                             <label value="Name" type="text">Bio</label><br />
-                            <textarea htmlFor="userBio"
+                            <textarea 
+                                htmlFor="userBio"
                                 placeholder="Enter your bio here...."
                                 type="text" rows="10" cols="20"
-                                id="userBio" required
-                            //onchange={event => { setBio(event.target.value); }}
+                                id="userBio" 
+                                value={bio}
+                                onChange={event => { setBio(event.target.value); }}
                             />
                         </div>
                         <input className="submitBtn" value="submit" type="submit" />
                         <input className="submitBtn" value="Edit" type="submit" />
                     </div>
                 </form>
-                <ul>
-                { <ListReviewgit s username={props.activeUser.username} />}
-                </ul>
-            </div>
+
+                
+                 <ListReviews username={props.activeUser.username} />
+                
+            </>
              
         );
+    }
+    else return (
+        <Redirect to="/" />
+    );
     
     }
 
