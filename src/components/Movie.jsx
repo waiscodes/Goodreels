5780454afe6eba53af20c7ac375ea7986e2cd830
waiscodes <b.wais@outlook.com>
2020-09-26 13:52:08 -0600

    added a div to create a side by side view of image with info

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,58 +1,62 @@
-import React from 'react';
-import AddReview from './AddReview';
-import ListReviews from './ListReviews';
-import { connect } from 'react-redux';
-import {avgRating} from '../utilities/avgRating';
-import {getRatingClass} from '../utilities/getRatingClass';
-import { Redirect } from 'react-router-dom';
+import React from "react";
+import AddReview from "./AddReview";
+import ListReviews from "./ListReviews";
+import { connect } from "react-redux";
+import { avgRating } from "../utilities/avgRating";
+import { getRatingClass } from "../utilities/getRatingClass";
+import { Redirect } from "react-router-dom";
 
-import '../css/Movie.css';
-
-
+import "../css/Movie.css";
 
 const Movie = (props) => {
+  // receives props.movieId as prop
 
-    // receives props.movieId as prop
+  let thisMovie;
+  let reviewsCopy = [...props.reviews];
+  let averageRating;
+  let colorClass;
 
-    let thisMovie;
-    let reviewsCopy = [...props.reviews];
-    let averageRating;
-    let colorClass;
-
-    // Find the movie with matching ID to pull out info from store into my variable
-    for (const movie of props.movies){
-
-        if (movie.id === props.activeUser.clickedMovie) {
-            thisMovie = {...movie};
-        }
-
-        if (!thisMovie) thisMovie = props.movies[props.movies.length-1];
-
+  // Find the movie with matching ID to pull out info from store into my variable
+  for (const movie of props.movies) {
+    if (movie.id === props.activeUser.clickedMovie) {
+      thisMovie = { ...movie };
     }
 
-    // Calculates average movie rating and assigns class based on it
-    averageRating = avgRating(thisMovie.id, reviewsCopy);
-    colorClass = getRatingClass(averageRating);
+    if (!thisMovie) thisMovie = props.movies[props.movies.length - 1];
+  }
 
+  // Calculates average movie rating and assigns class based on it
+  averageRating = avgRating(thisMovie.id, reviewsCopy);
+  colorClass = getRatingClass(averageRating);
 
-    // Return HTML element with movie's info
-    if (props.activeUser.username !== undefined){
-        return (
-            <>
+  // Return HTML element with movie's info
+  if (props.activeUser.username !== undefined) {
+    return (
+      <>
+        <div className="imginfo">
+          <div className="info">
+            <h2>{thisMovie.title}</h2>
+            <em>{thisMovie.year}</em>
+            <p id="rating">{averageRating}</p>
+          </div>
 
-                <h2>{thisMovie.title}</h2>
-                <em>{thisMovie.year}</em>
-                <div id="rating">{averageRating}</div>
-                <img src={require('../img/'+thisMovie.image)}alt="Movie Poster" className={colorClass} />
-                <p>{thisMovie.genre}</p>
-                <p>{thisMovie.synopsis}</p>
+          <img
+            src={require("../img/" + thisMovie.image)}
+            alt="Movie Poster"
+            className={colorClass}
+          />
+        </div>
+        <p>{thisMovie.genre}</p>
+        <p>{thisMovie.synopsis}</p>
 
-                <AddReview />
-                <ListReviews movieId={thisMovie.id} />
-                
-            </>
-        );
-    }
-    else return <Redirect to="/" />
-}
-export default connect( state => ({movies: state.movies, reviews: state.reviews, activeUser: state.activeUser}) )(Movie);
+        <AddReview />
+        <ListReviews movieId={thisMovie.id} />
+      </>
+    );
+  } else return <Redirect to="/" />;
+};
+export default connect((state) => ({
+  movies: state.movies,
+  reviews: state.reviews,
+  activeUser: state.activeUser,
+}))(Movie);
