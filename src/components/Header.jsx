e3efd265dfa907b14a1541ba9eb567b70f82620c
waiscodes <b.wais@outlook.com>
2020-09-29 18:41:47 -0600

    added a link to the logo to linking to browse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Nav from "./Nav";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-
   return (
     <>
       <header>
-        
-        <h1 className="logo">good<strong className="logo-reels">reels</strong></h1>
-        <Nav />
+        <Link to="/Browse">
+          <h1 className="logo">
+            good<strong className="logo-reels">reels</strong>
+          </h1>
+        </Link>
 
+        <Nav />
       </header>
     </>
   );
