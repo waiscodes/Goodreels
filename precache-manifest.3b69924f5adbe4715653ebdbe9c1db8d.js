fbec5b79e593b59bfe8cb37188f6ffd1c67d15ec
waiscodes <b.wais@outlook.com>
2020-11-07 13:55:52 -0700

    Updates

---
+++ precache-manifest.3b69924f5adbe4715653ebdbe9c1db8d.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88119a5cb30bc939118ecf8e8e7ff38f",
+    "url": "/Goodreels/index.html"
+  },
+  {
+    "revision": "3da53fecc5d0f1df68e1",
+    "url": "/Goodreels/static/css/main.0b3caa03.chunk.css"
+  },
+  {
+    "revision": "f5882d32f39f9f9d8993",
+    "url": "/Goodreels/static/js/2.c708f351.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Goodreels/static/js/2.c708f351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da53fecc5d0f1df68e1",
+    "url": "/Goodreels/static/js/main.5085ee7e.chunk.js"
+  },
+  {
+    "revision": "61caf102caf0d22f4648",
+    "url": "/Goodreels/static/js/runtime-main.ea77cf57.js"
+  },
+  {
+    "revision": "4d2f63bb06592ee54ece6b5d48947b1e",
+    "url": "/Goodreels/static/media/13hrs.4d2f63bb.jpg"
+  },
+  {
+    "revision": "5b3a7a6fdad180274741546a7b86817e",
+    "url": "/Goodreels/static/media/Joker.5b3a7a6f.jpg"
+  },
+  {
+    "revision": "476155a4c95b8e364051efaefc677264",
+    "url": "/Goodreels/static/media/Parasite.476155a4.jpg"
+  },
+  {
+    "revision": "d974855e86f66f0be1fd9bcd630bce07",
+    "url": "/Goodreels/static/media/annihilation.d974855e.jpg"
+  },
+  {
+    "revision": "d446a7c6dc1b9ec3822e74ed75f4cedd",
+    "url": "/Goodreels/static/media/arrival.d446a7c6.jpg"
+  },
+  {
+    "revision": "f28923e4b5f8e6c11079e376a2383b3c",
+    "url": "/Goodreels/static/media/braveheart.f28923e4.jpg"
+  },
+  {
+    "revision": "9cb69eee2c770195ba4e68956911111e",
+    "url": "/Goodreels/static/media/catchmeifyoucan.9cb69eee.jpg"
+  },
+  {
+    "revision": "e8706b15584ef3a6baacacc905aaa49d",
+    "url": "/Goodreels/static/media/click.e8706b15.jpg"
+  },
+  {
+    "revision": "1c691f31700b5133cf737f635a155cce",
+    "url": "/Goodreels/static/media/coriolanus.1c691f31.jpg"
+  },
+  {
+    "revision": "bc0aee7f3a47546827f94ab63b2eb4b2",
+    "url": "/Goodreels/static/media/deadpoetssociety.bc0aee7f.jpg"
+  },
+  {
+    "revision": "36ebd0b38cd78e084d3b81dbb53f5819",
+    "url": "/Goodreels/static/media/devilallthetime.36ebd0b3.jpg"
+  },
+  {
+    "revision": "74eb87246ca283bd246439aa23f9bb72",
+    "url": "/Goodreels/static/media/enola.74eb8724.jpg"
+  },
+  {
+    "revision": "281eaa6e364aec3374691453e8877c06",
+    "url": "/Goodreels/static/media/gladiator.281eaa6e.jpg"
+  },
+  {
+    "revision": "2a06b3c98602bb60d401aaf2e4c6a575",
+    "url": "/Goodreels/static/media/gonegirl.2a06b3c9.jpg"
+  },
+  {
+    "revision": "f0c2dc70268cb6958b635e114250d52f",
+    "url": "/Goodreels/static/media/goodfellas.f0c2dc70.jpg"
+  },
+  {
+    "revision": "9877389866989c7b8fba1bfe9dbed71d",
+    "url": "/Goodreels/static/media/hackshawridge.98773898.jpg"
+  },
+  {
+    "revision": "b8e922214a7e20e8b86e1c79d3dc98b8",
+    "url": "/Goodreels/static/media/ironman.b8e92221.jpg"
+  },
+  {
+    "revision": "f270c11fbdb947b590f166e7d2aeadac",
+    "url": "/Goodreels/static/media/it.f270c11f.jpg"
+  },
+  {
+    "revision": "40523b21813580cb49647fdead9f4c23",
+    "url": "/Goodreels/static/media/machina.40523b21.jpg"
+  },
+  {
+    "revision": "7caeaaad60969b5f058ae36ec1133e3e",
+    "url": "/Goodreels/static/media/matrix.7caeaaad.jpg"
+  },
+  {
+    "revision": "00563b8ac947123ae377dc5dad41c24f",
+    "url": "/Goodreels/static/media/mrsmith.00563b8a.jpg"
+  },
+  {
+    "revision": "3dcaad913435a60eaf7eb5869ef87ce6",
+    "url": "/Goodreels/static/media/pay.3dcaad91.jpg"
+  },
+  {
+    "revision": "77998f2bfc834212d975e9e0a0685bfc",
+    "url": "/Goodreels/static/media/pofh.77998f2b.jpg"
+  },
+  {
+    "revision": "ab18f40693429de12e338e5ebdc48005",
+    "url": "/Goodreels/static/media/postcard.ab18f406.jpg"
+  },
+  {
+    "revision": "3571bb87034df450d9bc9c06b84bcdde",
+    "url": "/Goodreels/static/media/quietplace.3571bb87.jpg"
+  },
+  {
+    "revision": "7cecc04bb6aa226254de8e232c1e68fc",
+    "url": "/Goodreels/static/media/reddawn.7cecc04b.jpg"
+  },
+  {
+    "revision": "dd73e29e065eca4ad3edc716fa194500",
+    "url": "/Goodreels/static/media/sc.dd73e29e.jpg"
+  },
+  {
+    "revision": "f75d4773e285a9529491f3408cb4bca5",
+    "url": "/Goodreels/static/media/shawshankredemption.f75d4773.jpg"
+  },
+  {
+    "revision": "3d8d62c2d953d2979caf78c2bfe8d624",
+    "url": "/Goodreels/static/media/spr.3d8d62c2.jpg"
+  },
+  {
+    "revision": "7be2ed6aa5068e744e56a27dca9611c4",
+    "url": "/Goodreels/static/media/t-a-b.7be2ed6a.jpg"
+  },
+  {
+    "revision": "2a5d262bfe9d23d7517ca189cc69c0ec",
+    "url": "/Goodreels/static/media/theateam.2a5d262b.jpg"
+  },
+  {
+    "revision": "ee50c99bd8cf8a66bf1b478adea0a6bd",
+    "url": "/Goodreels/static/media/thebanker.ee50c99b.jpg"
+  },
+  {
+    "revision": "e8f50286a0dd5b4d622bc2970a0e3852",
+    "url": "/Goodreels/static/media/thepatriot.e8f50286.jpg"
+  },
+  {
+    "revision": "33ba111932d6d374dab82d47d9845317",
+    "url": "/Goodreels/static/media/truelies.33ba1119.jpg"
+  },
+  {
+    "revision": "9d810bf1a977de8dfbcfbbc3cf408209",
+    "url": "/Goodreels/static/media/zohan.9d810bf1.jpg"
+  }
+]);
